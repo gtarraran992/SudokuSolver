e87b040e66aa44034f921544e80c84c879d7f7fa
Giacomo <gtarraran992@gmail.com>
2026-05-08 16:29:56 +0200

    capacitor-android

---
+++ android/app/src/main/java/com/giacomo/sudokuhint/MainActivity.java
@@ -0,0 +1,5 @@
+package com.giacomo.sudokuhint;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.giacomo.sudokuhint',
+  appName: 'SudokuHint',
+  webDir: 'dist'
+};
+
+export default config;
